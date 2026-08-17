bd922e9fe13ff9d160b18789de0097735add94d5
dsluizfreitas <ds.luizfreitas@gmail.com>
2026-08-17 09:45:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='simuguia-v29';
+var CACHE='simuguia-v30';
 var ASSETS=['./','./index.html','./manifest.webmanifest','./favicon.png','./privacidade.html',
 './icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',function(e){
